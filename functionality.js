9d7930ed8000a6f84f8275c2cae208c23bcb79d8
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2022-03-20 22:15:28 -0700

    redesign navbar from mobile first approach. Corrected spacing of navbar items at desktop display

--- functionality.js
+++ functionality.js
@@ -1,8 +1,8 @@
     function openMenu() {
       var x = document.getElementById("navbar");
-      if (x.className === "navbar") {
+      if (x.className === "navbar row") {
         x.className += " mobile";
       } else {
-        x.className = "navbar";
+        x.className = "navbar row";
       }
     }
