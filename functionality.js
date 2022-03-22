261a3e9fb01ec54242e84930116e12e0844be50c
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2022-03-21 18:11:49 -0700

    updated javascript file to have two functions to open and close the nav menu on mobile screen. The icon changes from hamburger to an x. updated CSS file to reflect new class names

--- functionality.js
+++ functionality.js
@@ -1,8 +1,38 @@
-    function openMenu() {
-      var x = document.getElementById("navbar");
-      if (x.className === "navbar row") {
-        x.className += " mobile";
-      } else {
-        x.className = "navbar row";
-      }
-    }
+let open = document.getElementById("open");
+let close = document.getElementById("close");
+let nav = document.getElementById("navbar");
+  
+function openMenu() {
+    nav.className = "navbar row mobile";
+    open.className = "navbar__hamburger-menu close";
+    close.className = "navbar__hamburger-menu open";
+
+}
+
+function closeMenu() {
+  nav.className = "navbar row";
+  open.className = "navbar__hamburger-menu open";
+  close.className = "navbar__hamburger-menu close";
+
+}
+
+// function openMenu() {
+//   var open = document.getElementById("open");
+//   var close = document.getElementById("close");
+//   var nav = document.getElementById("navbar");
+
+//   if (nav.className === "navbar row") {
+//     nav.className += " mobile";
+//     open.className = "navbar__hamburger-menu close";
+//     close.className = "navbar__hamburger-menu open";
+//   }
+//   else {
+//     nav.className = "navbar row"
+//     open.className = "navbar__hamburger-menu open";
+//     close.className = "navbar__hamburger-menu close";
+//   }
+      
+// }
+    
+
+
